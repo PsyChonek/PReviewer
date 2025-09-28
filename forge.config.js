58d4f46d778a46baba34b3c5b11966c959fe68bb
Daniel Vazač <daniel.vazac@email.cz>
2025-09-28 16:14:48 +0200

    Build CI

--- forge.config.js
+++ forge.config.js
@@ -26,6 +26,30 @@ module.exports = {
   ],
   plugins: [
     {
+      name: '@electron-forge/plugin-vite',
+      config: {
+        // `build` can specify multiple entry builds, which can be Main process, Preload scripts, Worker process, etc.
+        // If you are familiar with Vite configuration, it will look really familiar.
+        build: [
+          {
+            // `entry` is just an alias for `build.lib.entry` in the corresponding file of `config`.
+            entry: 'src/main.js',
+            config: 'vite.main.config.js',
+          },
+          {
+            entry: 'src/preload.js',
+            config: 'vite.preload.config.js',
+          },
+        ],
+        renderer: [
+          {
+            name: 'main_window',
+            config: 'vite.renderer.config.js',
+          },
+        ],
+      },
+    },
+    {
       name: '@electron-forge/plugin-auto-unpack-natives',
       config: {},
     },
